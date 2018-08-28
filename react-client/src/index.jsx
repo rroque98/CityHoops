ffc734fdd3dfc7d6e5529a5341df1984cd6cca2b
Ricardo <rroque98@gmail.com>
2018-08-28 14:24:41 -0700

    Implement top app structure

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import axios from 'axios';
+import GamesList from './components/GamesList.jsx';
+import GoogleMap from './components/GoogleMap.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
+    this.state = {
+      games: [],
+      users: [],
     }
+    this.handleClickJoin = this.handleClickJoin.bind(this);
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
+    axios.get('/games')
+      .then((response) => {
+        console.log(response.data);
+        for (let i = 0; i < response.data.length; i++) {
+          let adjustedDay =response.data[i].day.substring(0, 3);
+          response.data[i].day = adjustedDay;
+        }
         this.setState({
-          items: data
+          games: response.data,
         })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios.get('/users')
+      .then((users) => {
+        console.log(users.data);
+        this.setState({
+          users: users.data,
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleClickJoin(e, id) {
+    let data = { user_id: 21, basketballgame_id: id};
+    $.ajax({
+      type: "POST",
+      url: '/joinGame',
+      data: data,
+      success: ''
     });
+
+    axios.get('/users')
+      .then((users) => {
+        console.log(users.data);
+        this.setState({
+          users: users.data,
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+    return (
+      <div>
+        <h1><i className="fas fa-basketball-ball"></i> <strong>Ballers</strong></h1>
+        <h6 className="">  An app for anyone looking to play basketball and meet new people.</h6>
+
+        <div className='google-map-wrapper'>
+          <GoogleMap />
+        </div>
+        <div>
+          <GamesList games={this.state.games} handleClickJoin={this.handleClickJoin} users={this.state.users}/>
+        </div>
+      </div>
+    )
   }
 }
 
