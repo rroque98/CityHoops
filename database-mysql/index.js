2ba91338d183b57951dd64716494d53c2e908dd3
Ricardo <rroque98@gmail.com>
2018-08-28 14:21:16 -0700

    Add database query logic

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,14 +1,15 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'FILL_ME_IN',
-  database : 'test'
+  password : '',
+  database : 'ballers'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
+const selectAllUsers = function(callback) {
+  const query = `SELECT * FROM users INNER JOIN users_basketballgames ON (users_basketballgames.users_id = users.id)`;
+  connection.query(query, (err, results) => {
     if(err) {
       callback(err, null);
     } else {
@@ -17,4 +18,41 @@ var selectAll = function(callback) {
   });
 };
 
-module.exports.selectAll = selectAll;
+const selectSpecificGame = function(bBallGameId, callback) {
+  const query = `SELECT * FROM users INNER JOIN basketballgames ON users.bbgame_id = basketballgames.id WHERE basketballgames.id = ${bBallGameId}`;
+  connection.query(query, function(err, results) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+const selectAllGames = function(callback) {
+  const query = `SELECT * FROM basketballgames`;
+  connection.query(query, function(err, results) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+const joinGame = (data, callback) => {
+  const query = `INSERT INTO users_basketballgames (users_id, basketballgames_id) VALUES ('${Number(data.user_id)}', '${Number(data.basketballgame_id)}')`;
+  connection.query(query, function(err, results) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+module.exports.selectAllUsers = selectAllUsers;
+module.exports.selectAllGames = selectAllGames;
+module.exports.selectSpecificGame = selectSpecificGame;
+module.exports.joinGame = joinGame;
+module.exports.connection = connection;
