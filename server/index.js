0bd0e90ceb503fd3ec43640628437f9d92a3e2f8
Ricardo <rroque98@gmail.com>
2018-08-28 14:22:00 -0700

    Implement request handlers in server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../database-mysql');
 
-var app = express();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+bodyParser.json();
+app.use(express.static(__dirname + '/../react-client/dist'));
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.get('/users', function (req, res) {
+  db.selectAllUsers(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
+app.get('/games', function (req, res) {
+  db.selectAllGames(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
+app.get('/specificGame', function (req, res) {
+  db.selectSpecificGame(req.body.gameId, function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.post('/joinGame', function (req, res) {
+  db.joinGame(req.body, function(err, data) {
     if(err) {
       res.sendStatus(500);
     } else {
